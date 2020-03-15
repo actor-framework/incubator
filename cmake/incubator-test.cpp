3d945be176f15cfc2b75eb36c5befcf5a45042db
Jakob Otto <jakob.otto@haw-hamburg.de>
2020-03-15 16:26:51 +0100

    Port libcaf_net code

--- cmake/incubator-test.cpp
+++ cmake/incubator-test.cpp
@@ -1,1 +1,9 @@
+#define CAF_TEST_NO_MAIN
+
+#include "caf/net/test/incubator_test.hpp"
 #include "caf/test/unit_test_impl.hpp"
+
+int main(int argc, char** argv) {
+  caf::init_global_meta_objects<>();
+  return caf::test::main(argc, argv);
+}
